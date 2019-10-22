cf0f85d7e05881cec20a523152d8c0f9a3c26347
newberts <newberts@MCD210-PC-12.uccc.uc.edu>
2019-10-22 11:31:36 -0400

    I added a method

--- MyFirstGitHubRepo/src/main/Main.java
+++ MyFirstGitHubRepo/src/main/Main.java
@@ -3,8 +3,13 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 
 	}
-
+	/***
+	 * A method that prints Travis Newberry
+	 */
+	public static void newberts() {
+		System.out.println("Travis Newberry");
+	}
 }
