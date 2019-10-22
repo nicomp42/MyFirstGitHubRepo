d1deb359fe19a3ebfb1c2f6fcfd38c5556d82450
stratmtr <stratmtr@MCD210-PC-11.uccc.uc.edu>
2019-10-22 11:31:47 -0400

    My 1st commit

--- MyFirstGitHubRepo/src/main/Main.java
+++ MyFirstGitHubRepo/src/main/Main.java
@@ -3,8 +3,10 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-
+		stratmtr();
 
 	}
-
+	public static void stratmtr()	{
+		System.out.println("Tyler Stratman");
+	}
 }
